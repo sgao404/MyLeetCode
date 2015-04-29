f0f067858bd93e7d065c4f0fe824d4227c08f2a3
Sida Gao <gaosida@bu.edu>
2015-04-29 00:05:37 -0400

    #035 Search Insert Position
    https://leetcode.com/problems/search-insert-position/

---
+++ #035 Search Insert Position/java.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution { // recursively （take from leetcode)
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) return null;
+        head.next = removeElements(head.next, val);
+        return head.val == val ? head.next : head;
+    }
+}
+
+public class Solution { // iteratively
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode top = new ListNode(1111); 
+        top.next = head;
+        
+        ListNode pre = top; 
+        ListNode curr = head;
+        while (curr!=null){
+            if(curr.val==val){
+                pre.next=curr.next;
+                curr=curr.next;
+            } else{
+            pre=pre.next;
+            curr=curr.next;
+            }
+        }   
+        return top.next;
+    }
+};
