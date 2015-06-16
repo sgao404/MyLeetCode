931263cdef96228d1caa1a6a1777f431fdc6aabb
Sida Gao <gaosida@bu.edu>
2015-06-15 23:52:55 -0400

    #226 Invert Binary Tree
    https://leetcode.com/problems/invert-binary-tree/

--- #226 Invert Binary Tree/c++.cpp
+++ #226 Invert Binary Tree/c++.cpp
@@ -7,7 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution { // recursively 
+class Solution { // recursive
 public:
     TreeNode* invertTree(TreeNode* root) {
         if (root) {
@@ -17,5 +17,24 @@ public:
         }
         return root;
         
+    }
+};
+
+class Solution {  // non-recursive
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        std::stack<TreeNode*> stack;
+        stack.push(root);
+        
+        while(!stack.empty()) {
+            TreeNode* temp = stack.top();
+            stack.pop();
+            if (temp) {
+                stack.push(temp->left);
+                stack.push(temp->right);
+                std:swap(temp->left,temp->right);
+            }
+        }
+        return root;
     }
 };
--- #226 Invert Binary Tree/java.java
+++ #226 Invert Binary Tree/java.java
@@ -7,12 +7,38 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-public class Solution { // recursively
+public class Solution { // recursive
     public TreeNode invertTree(TreeNode root) {
         if (root != null) {
         invertTree(root.left);
         invertTree(root.right);
         swap(root);
+        }
+        return root;
+
+    }
+    
+    public void swap(TreeNode root) {
+        TreeNode temp = new TreeNode(1);
+        temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+    }
+}
+
+public class Solution { // non-recursive
+    public TreeNode invertTree(TreeNode root) {
+        stack<TreeNode> stack = new stack<TreeNode>();
+        stack.push(root);
+        
+        while(!stack.empty()) {
+            TreeNode temp = stack.front();
+            stack.pop();
+            if (temp) {
+                stack.push(temp.left);
+                stack.push(temp.right);
+                swap(temp.left,temp.right);
+            }
         }
         return root;
 
